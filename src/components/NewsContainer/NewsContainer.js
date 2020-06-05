8d99dd70ba70df450ba26455b4fa2609eb36edbf
Elom Amouzou <elom1990@yahoo.com>
2020-06-04 19:14:11 -0600

    use debugger to determine contents of local

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './NewsContainer.css'
-
+import local from '../../data/local.js'
 // NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = () => {
+    const art = [ local ]
+    debugger;
+}
 
 export default NewsContainer;
