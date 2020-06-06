fb3d4558cd2b243a41d3a40f7e03de350c34826f
Elom Amouzou <elom1990@yahoo.com>
2020-06-05 23:03:46 -0600

    create and return article constant

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -3,14 +3,17 @@ import './NewsArticle.css';
 
 // NEWSARTICLE COMPONENT CODE GOES HERE
 const NewsArticle = (props) => {
-  
-  return (
+  const article = (
     <div>
       <h1>{props.info.headline}</h1>
       <p>{props.info.description}</p>
       <h3><a href={props.info.url}>Article Link</a></h3>
       <img src={props.info.img}></img>
     </div>
+  )
+  
+  return (
+    article
   )
 }
 export default NewsArticle;
