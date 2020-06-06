eb8b3ae7a856bd8a7e60964bd23c57d588923a58
Elom Amouzou <elom1990@yahoo.com>
2020-06-05 22:15:12 -0600

    use debugger to determine contents of props

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -2,5 +2,11 @@ import React from 'react';
 import './NewsArticle.css';
 
 // NEWSARTICLE COMPONENT CODE GOES HERE
+const NewsArticle = (props) => {
+  return (
+    <div>
 
+    </div>
+  )
+}
 export default NewsArticle;
