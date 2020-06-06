df3b008ed7a757344bf8d3faf91a1cb3193d35e4
Elom Amouzou <elom1990@yahoo.com>
2020-06-06 17:09:51 -0600

    import data into app state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer.js'
 
@@ -7,11 +11,16 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local,
+      entertainment,
+      health,
+      science,
+      technology,
     }
   }
 
   render () {
+    
     return (
       <div className="app">
         <NewsContainer />
