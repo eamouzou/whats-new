87dd3ee9a65d57d4298176fc2fa229ca3eb75d68
Elom Amouzou <elom1990@yahoo.com>
2020-06-05 22:14:48 -0600

    map through local data using newsarticle component

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './NewsContainer.css'
 import local from '../../data/local.js'
+import NewsArticle from '../NewsArticle/NewsArticle.js'
+
 // NEWSCONTAINER COMPONENT CODE GOES HERE
 const NewsContainer = () => {
-    const art = [ local ]
-    debugger;
+  return (
+    <div>
+      {local.map(article => <NewsArticle info={article} />)}
+    </div>
+  )
 }
 
 export default NewsContainer;
