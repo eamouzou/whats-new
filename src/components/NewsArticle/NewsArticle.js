0e486803071295750cb9668c0c79cae80ce6364e
Elom Amouzou <elom1990@yahoo.com>
2020-06-05 22:23:42 -0600

    create NewsArticle component

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -3,9 +3,13 @@ import './NewsArticle.css';
 
 // NEWSARTICLE COMPONENT CODE GOES HERE
 const NewsArticle = (props) => {
+  
   return (
     <div>
-
+      <h1>{props.info.headline}</h1>
+      <p>{props.info.description}</p>
+      <h3><a href={props.info.url}>Article Link</a></h3>
+      <img src={props.info.img}></img>
     </div>
   )
 }
