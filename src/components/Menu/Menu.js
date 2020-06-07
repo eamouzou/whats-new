869e4f5a03dd38537f6946e708bdf458620e3f65
Elom Amouzou <elom1990@yahoo.com>
2020-06-07 13:39:18 -0600

    create menu component, disply category buttons

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,5 +2,13 @@ import React from 'react';
 import './Menu.css'
 
 // MENU COMPONENT CODE GOES HERE
-
+function Menu(props){
+  return(
+    <navbar>
+      <ul>
+      {props.news.map(category => <button>{category}</button>)}
+      </ul>
+    </navbar>
+  )
+}
 export default Menu;
